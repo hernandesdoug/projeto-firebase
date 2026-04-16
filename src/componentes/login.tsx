0ad1a7c2dd31cefa23ae743a97e3e9af2dcc3624
Douglas Hernandes <douglashernandes@icloud.com>
2026-04-16 16:46:58 -0300

    login com signIn Firebase e form antd

--- src/componentes/login.tsx
+++ src/componentes/login.tsx
@@ -1,12 +1,68 @@
-import { useState, useEffect } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../services/firebase.ts";
+import type { FieldType } from "./login.ts";
+import { Button, Checkbox, Form, Input } from 'antd';
+import type { FormProps } from 'antd';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../services/firebase.ts";
 
+ const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+    const { email, password } = values;
+      try {
+        const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+     console.log('Usuário logado:', userCredential.user);
+    } catch (error) {
+       console.error(error); 
+    }
+    
+};
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+  
 function Login() {
-
+   
     return (
-        <>
-        </>
+        <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+        >
+            <Form.Item<FieldType>
+                label="Email"
+                name="email"
+                rules={[{ required: true, message: 'Please input your email!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item<FieldType>
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
+                <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item label={null}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
     )
+
 }
+
 export default Login;
